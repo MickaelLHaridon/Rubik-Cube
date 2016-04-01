7854bd0d998e12a23e72837c6a0ae88966b9bd34
atombu <antoine.tombu@etudiant.univ-lr.fr>
2016-04-01 15:57:09 +0200

    Melanger

--- Rubik cube/Rubik_cube/GestionFace.cs
+++ Rubik cube/Rubik_cube/GestionFace.cs
@@ -18,6 +18,7 @@ namespace Rubik_cube
         KeyboardState oldKey;
         KeyboardState newKey;
         float[,] rotations;
+        Vector3[] vectRot;
 
         public GestionFace(Game game) :base(game)
         {
@@ -28,6 +29,7 @@ namespace Rubik_cube
             }
             rotations = new float[6, 2] { { DROITE, MathHelper.PiOver2 }, { GAUCHE, -MathHelper.PiOver2 }, { GAUCHE, MathHelper.PiOver2 },
                 { DROITE, -MathHelper.PiOver2 }, { DROITE, -MathHelper.PiOver2 }, { DROITE, MathHelper.PiOver2 } };
+            vectRot = new Vector3[6] { Vector3.Forward, Vector3.Right, Vector3.Backward, Vector3.Left, Vector3.Down, Vector3.Up };
         }
 
         public override void Initialize()
@@ -83,12 +85,27 @@ namespace Rubik_cube
             }
         }
 
-        public void Melanger()
+        public void Melanger(int nombre)
         {
-            int nb = rand.Next(0, 6);
-            int direction = rand.Next(0, 2);
-            faces[nb].TranslateFace(rand.Next(0, 2));
-           
+            for(int i = 0; i < nombre; i++)
+            {
+                int nb = rand.Next(0, 6);
+                int direction = rand.Next(0, 2);
+                if(direction == DROITE)
+                {
+                    faces[nb].TranslateFace(rotations[nb, 0]);
+                    faces[nb].RotationFace(vectRot[nb], rotations[nb, 1]);
+                }
+                else
+                {
+                    faces[nb].TranslateFace(Math.Abs(rotations[nb, 0]-1));
+                    faces[nb].RotationFace(vectRot[nb], -rotations[nb, 1]);
+                }
+                UpdateFaces();
+
+             }
+
+
         }
 
         public override void Update(GameTime gameTime)
@@ -96,7 +113,11 @@ namespace Rubik_cube
             oldKey = newKey;
             newKey = Keyboard.GetState();
 
-            if(newKey.IsKeyDown(Keys.LeftShift) && oldKey.IsKeyUp(Keys.LeftShift))
+            if (newKey.IsKeyDown(Keys.Space) && oldKey.IsKeyUp(Keys.Space))
+            {
+                Melanger(50);
+            }
+            if (newKey.IsKeyDown(Keys.LeftShift) && oldKey.IsKeyUp(Keys.LeftShift))
             {
                 if (leftShift)
                     leftShift = false;
@@ -109,39 +130,39 @@ namespace Rubik_cube
                 if (newKey.IsKeyDown(Keys.NumPad1) && oldKey.IsKeyUp(Keys.NumPad1))
                 {
                     faces[0].TranslateFace(Math.Abs(rotations[0, 0]-1));
-                    faces[0].RotationFace(Vector3.Forward, -rotations[0, 1]);
+                    faces[0].RotationFace(vectRot[0], -rotations[0, 1]);
                     UpdateFaces();
                 }
 
                 if (newKey.IsKeyDown(Keys.NumPad2) && oldKey.IsKeyUp(Keys.NumPad2))
                 {
                     faces[1].TranslateFace(Math.Abs(rotations[1, 0]-1));
-                    faces[1].RotationFace(Vector3.Right, -rotations[1, 1]);
+                    faces[1].RotationFace(vectRot[1], -rotations[1, 1]);
                     UpdateFaces();
                 }
                 if (newKey.IsKeyDown(Keys.NumPad3) && oldKey.IsKeyUp(Keys.NumPad3))
                 {
                     faces[2].TranslateFace(Math.Abs(rotations[2, 0]-1));
-                    faces[2].RotationFace(Vector3.Backward, -rotations[2, 1]);
+                    faces[2].RotationFace(vectRot[2], -rotations[2, 1]);
                     UpdateFaces();
                 }
 
                 if (newKey.IsKeyDown(Keys.NumPad4) && oldKey.IsKeyUp(Keys.NumPad4))
                 {
                     faces[3].TranslateFace(Math.Abs(rotations[3, 0]-1));
-                    faces[3].RotationFace(Vector3.Left, -rotations[3, 1]);
+                    faces[3].RotationFace(vectRot[3], -rotations[3, 1]);
                     UpdateFaces();
                 }
                 if (newKey.IsKeyDown(Keys.NumPad5) && oldKey.IsKeyUp(Keys.NumPad5))
                 {
                     faces[4].TranslateFace(Math.Abs(rotations[4, 0]-1));
-                    faces[4].RotationFace(Vector3.Down, -rotations[4, 1]);
+                    faces[4].RotationFace(vectRot[4], -rotations[4, 1]);
                     UpdateFaces();
                 }
                 if (newKey.IsKeyDown(Keys.NumPad6) && oldKey.IsKeyUp(Keys.NumPad6))
                 {
                     faces[5].TranslateFace(Math.Abs(rotations[5, 0]-1));
-                    faces[5].RotationFace(Vector3.Up, -rotations[5, 1]);
+                    faces[5].RotationFace(vectRot[5], -rotations[5, 1]);
                     UpdateFaces();
                 }
             }
@@ -150,39 +171,39 @@ namespace Rubik_cube
                 if (newKey.IsKeyDown(Keys.NumPad1) && oldKey.IsKeyUp(Keys.NumPad1))
                     {
                         faces[0].TranslateFace(rotations[0, 0]);
-                        faces[0].RotationFace(Vector3.Forward, rotations[0, 1]);
+                        faces[0].RotationFace(vectRot[0], rotations[0, 1]);
                         UpdateFaces();
                     }
                 
                 if (newKey.IsKeyDown(Keys.NumPad2) && oldKey.IsKeyUp(Keys.NumPad2))
                     {
                         faces[1].TranslateFace(rotations[1,0]);
-                        faces[1].RotationFace(Vector3.Right, rotations[1, 1]);
+                        faces[1].RotationFace(vectRot[1], rotations[1, 1]);
                         UpdateFaces();
                     }
                 if (newKey.IsKeyDown(Keys.NumPad3) && oldKey.IsKeyUp(Keys.NumPad3))
                 {
                     faces[2].TranslateFace(rotations[2, 0]);
-                    faces[2].RotationFace(Vector3.Backward, rotations[2, 1]);
+                    faces[2].RotationFace(vectRot[2], rotations[2, 1]);
                     UpdateFaces();
                 }
 
                 if (newKey.IsKeyDown(Keys.NumPad4) && oldKey.IsKeyUp(Keys.NumPad4))
                 {
                     faces[3].TranslateFace(rotations[3, 0]);
-                    faces[3].RotationFace(Vector3.Left, rotations[3, 1]);
+                    faces[3].RotationFace(vectRot[3], rotations[3, 1]);
                     UpdateFaces();
                 }
                 if (newKey.IsKeyDown(Keys.NumPad5) && oldKey.IsKeyUp(Keys.NumPad5))
                 {
                     faces[4].TranslateFace(rotations[4, 0]);
-                    faces[4].RotationFace(Vector3.Down, rotations[4, 1]);
+                    faces[4].RotationFace(vectRot[4], rotations[4, 1]);
                     UpdateFaces();
                 }
                 if (newKey.IsKeyDown(Keys.NumPad6) && oldKey.IsKeyUp(Keys.NumPad6))
                 {
                     faces[5].TranslateFace(rotations[5, 0]);
-                    faces[5].RotationFace(Vector3.Up, rotations[5, 1]);
+                    faces[5].RotationFace(vectRot[5], rotations[5, 1]);
                     UpdateFaces();
                 }
             }
